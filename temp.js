4e3922b3517b66197a9f8ca60d6f5b924c569c68
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-13 16:24:08 +0100

    upload

---
+++ temp.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'file-storage');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()} - ${file.originalname}`);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  const fileTypes = /jpeg|jpg|png|webp/;
+  const mimetype = fileTypes.test(file.mimetype);
+  const extname = fileTypes.test(
+    path.extname(file.originalname).toLocaleLowerCase()
+  );
+  if (mimetype && extname) {
+    return cb(null, true);
+  }
+  const err = `Error: file upload only support the following filetypes: ${fileTypes}`;
+  console.error(err);
+  cb(err);
+  return null;
+};
+const uploadFile = multer({ storage, fileFilter }).single('picture');
+module.exports = uploadFile;
